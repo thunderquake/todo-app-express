cd13f4e918fa4c3e0c96b2b68bb21f2ab56eb4b8
Anna Dmytriieva <loooverrr12@gmail.com>
2024-07-06 13:46:05 +0300

    feat: add a server folder

---
+++ server/src/constants/constants.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  TODOS_NOT_FOUND: "No todos found",
+  TODO_NOT_FOUND: "Todo not found",
+  TODO_CREATION_FAILED: "Failed to create todo",
+  TODO_UPDATE_FAILED: "Failed to update todo",
+  TODO_DELETION_FAILED: "Failed to delete todo",
+};
