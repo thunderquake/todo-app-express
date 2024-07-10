1012a0c4b406a0ad9bfa3e7583851857ed344065
Anna Dmytriieva <loooverrr12@gmail.com>
2024-07-10 16:36:32 +0300

    feat: add input modal

--- client/src/components/TodoTable.tsx
+++ client/src/components/TodoTable.tsx
@@ -6,10 +6,14 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import { TABLE_HEADERS } from "../constants/constants";
 import { Todo } from "../pages/TodosTablePage";
 import TodoRows from "./TodoRows";
+
+import { InputFormModal } from "./InputFormModal";
 
 export interface TodosTableProps {
   data: Todo[];
@@ -17,28 +21,42 @@ export interface TodosTableProps {
 
 const TodosTable = ({ data }: TodosTableProps) => {
   return (
-    <TableContainer
-      component={Paper}
-      sx={{ boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)", borderRadius: "4px" }}
-    >
-      <Table sx={{ minWidth: 650, maxHeight: 800 }} stickyHeader>
-        <TableHead>
-          <TableRow>
-            {TABLE_HEADERS.map((header) => (
-              <TableCell
-                key={header}
-                sx={{ fontWeight: 700, fontSize: "16px" }}
-              >
-                {header}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TodoRows data={data} />
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer
+        component={Paper}
+        sx={{ boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)", borderRadius: "4px" }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            fontWeight={700}
+            fontSize={"20px"}
+            sx={{ flexGrow: 1 }}
+          >
+            Todos
+          </Typography>
+          <InputFormModal />
+        </Toolbar>
+        <Table sx={{ minWidth: 650, maxHeight: 800 }} stickyHeader>
+          <TableHead>
+            <TableRow>
+              {TABLE_HEADERS.map((header) => (
+                <TableCell
+                  key={header}
+                  sx={{ fontWeight: 700, fontSize: "16px" }}
+                >
+                  {header}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TodoRows data={data} />
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
